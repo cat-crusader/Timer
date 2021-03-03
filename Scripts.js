06b590ee279ee792531df28196f5d9cba9f54d87
NIE-OleshZ <61271499+NIE-OleshZ@users.noreply.github.com>
2021-03-03 13:58:06 +0200

    First commit
    Timer written on js:
    .just simple timer
    .further there will be more updates and features

---
+++ Scripts.js
@@ -0,0 +1,21 @@
+// JavaScript source code
+//document.getElementById("demo").innerHTML = 5 + 6;
+/*
+function Show_Time(Date sec)
+{
+document.getElementById("demo").innerHTML=sec;
+}
+function Timer_Finish()
+{
+
+}
+function Timer(Date date)
+{
+  var date_sec = date.getSeconds();
+  for(int i=0;i<date_sec;i++)
+   {
+
+     setInterval(myAlert,1000);
+   }
+}
+*/
